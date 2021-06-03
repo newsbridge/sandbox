4a67c0fc4f3d50e30e5858bacfbc6cf637782c57
Olivier Truong <olivier.truong@newsbridge.io>
2021-06-03 09:44:34 +0200

    Setup python

---
+++ python/src/example.py
@@ -0,0 +1,14 @@
+def dummy_add(a, b):
+    if a:
+        if b:
+            result=a+b
+        else:
+            result=a
+    else:
+        result=0
+    c = 0
+    d = a + b
+    return d
+
+def dummy_multiply(c,d):
+    return c*d
