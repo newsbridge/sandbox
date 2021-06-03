553d1fdcd630e52677ff3311a8d8b361827b8610
Olivier Truong <olivier.truong@newsbridge.io>
2021-06-03 16:11:33 +0200

    Add new component

---
+++ typescript/src/app/newcomp/newcomp.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'te-newcomp',
+  templateUrl: './newcomp.component.html',
+  styleUrls: ['./newcomp.component.css']
+})
+export class NewcompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  let uselessVariable="";
+  }
+
+}
