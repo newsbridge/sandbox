88f2587952faf075c52b8a5f4c3ca2a071e775d1
Olivier Truong <olivier.truong@newsbridge.io>
2021-06-03 12:36:45 +0200

    duplicate file

---
+++ python/src/example2.py
@@ -0,0 +1,30 @@
+def dummy_add(a, b):
+    if a:
+        if b:
+            result=a+b
+        else:
+            result=a
+    else:
+        result=0
+    c = 0
+    d = a + b
+    return d
+
+def dummy_add2(a, b):
+    if a:
+        if b:
+            result=a+b
+        else:
+            result=a
+    else:
+        result=0
+    c = 0
+    d = a + b
+    return d
+
+def dummy_multiply(c,d):
+    return c*d
+
+def dummy_multiply2(c,d):
+    print("AGAIN")
+    return c*d
