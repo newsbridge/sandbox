5a05e58085d09704f5b3618218b50f4eacffb2bb
Olivier Truong <olivier.truong@newsbridge.io>
2021-06-03 12:34:13 +0200

    Add duplication

--- python/src/example.py
+++ python/src/example.py
@@ -10,6 +10,18 @@ def dummy_add(a, b):
     d = a + b
     return d
 
+def dummy_add2(a, b):
+    if a:
+        if b:
+            result=a+b
+        else:
+            result=a
+    else:
+        result=0
+    c = 0
+    d = a + b
+    return d
+
 def dummy_multiply(c,d):
     return c*d
 
