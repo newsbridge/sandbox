d8f27dd5bfc13a626e17eec68a1d607a4764b0f9
Olivier Truong <olivier.truong@newsbridge.io>
2021-06-04 14:46:35 +0200

    Test

--- python/src/example.py
+++ python/src/example.py
@@ -13,17 +13,10 @@ def dummy_add(a, b):
 def dummy_add2(a, b):
     return a + b
 
-def dummy_add3(a, b):
-    if a:
-        if b:
-            result=a+b
-        else:
-            result=a
-    else:
-        result=0
-    c = 0
-    d = a + b
-    return d
+def dummy_add3(first,second):
+    third=first+second
+    fourth=third-second
+    return fourth+second
 
 def dummy_multiply(c,d):
     return c*d
